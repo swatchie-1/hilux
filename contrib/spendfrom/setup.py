d7dcd7f1ef71c15d167e5cbb0cc362b81b0a6b22
Uranium Master <tzehming@gmail.com>
2018-07-20 06:15:26 +0000

    removed all

--- contrib/spendfrom/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='HLXspendfrom',
-      version='1.0',
-      description='Command-line utility for hilux "coin control"',
-      author='Gavin Andresen',
-      author_email='gavin@hiluxfoundation.org',
-      requires=['jsonrpc'],
-      scripts=['spendfrom.py'],
-      )
