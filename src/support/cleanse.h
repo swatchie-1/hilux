6bcd6e1f7e394bdf0372797c1e5e3eb58a1a4337
evilfatone <40913177+evilfatone@users.noreply.github.com>
2020-03-03 21:19:25 +0000

    Add files via upload

--- src/support/cleanse.h
+++ src/support/cleanse.h
@@ -3,11 +3,12 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef HILUX_SUPPORT_CLEANSE_H
-#define HILUX_SUPPORT_CLEANSE_H
+#ifndef BITCOIN_SUPPORT_CLEANSE_H
+#define BITCOIN_SUPPORT_CLEANSE_H
 
 #include <stdlib.h>
 
+// Attempt to overwrite data in the specified memory span.
 void memory_cleanse(void *ptr, size_t len);
 
-#endif // HILUX_SUPPORT_CLEANSE_H
+#endif // BITCOIN_SUPPORT_CLEANSE_H
