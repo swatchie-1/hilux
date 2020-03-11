ed7cd2e684b77d816c28a5955ddca19691168070
evilfatone <40913177+evilfatone@users.noreply.github.com>
2020-03-11 21:05:38 +0000

    updates

--- src/limitedmap.h
+++ src/limitedmap.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef HILUX_LIMITEDMAP_H
-#define HILUX_LIMITEDMAP_H
+#ifndef BITCOIN_LIMITEDMAP_H
+#define BITCOIN_LIMITEDMAP_H
 
 #include <assert.h>
 #include <map>
@@ -66,8 +66,11 @@ public:
     }
     void update(const_iterator itIn, const mapped_type& v)
     {
-        // TODO: When we switch to C++11, use map.erase(itIn, itIn) to get the non-const iterator.
-        iterator itTarget = map.find(itIn->first);
+        // Using map::erase() with empty range instead of map::find() to get a non-const iterator,
+        // since it is a constant time operation in C++11. For more details, see
+        // https://stackoverflow.com/questions/765148/how-to-remove-constness-of-const-iterator
+        iterator itTarget = map.erase(itIn, itIn);
+        
         if (itTarget == map.end())
             return;
         std::pair<rmap_iterator, rmap_iterator> itPair = rmap.equal_range(itTarget->second);
@@ -94,4 +97,4 @@ public:
     }
 };
 
-#endif // HILUX_LIMITEDMAP_H
+#endif // BITCOIN_LIMITEDMAP_H
