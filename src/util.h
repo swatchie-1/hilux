91e40ffc898c1589aa2230345cb5a4c0fed67c33
MadCatMining <zuklys@mail.ru>
2020-04-12 23:53:37 +0300

    Update util.h

--- src/util.h
+++ src/util.h
@@ -271,33 +271,22 @@ template <typename Callable> void TraceThread(const char* name,  Callable func)
     }
 }
 
-
-/**
- * @brief Converts version strings to 4-byte unsigned integer
- * @param strVersion version in "x.x.x" format (decimal digits only)
- * @return 4-byte unsigned integer, most significant byte is always 0
- * Throws std::bad_cast if format doesn\t match.
+/** Helper class to convert version strings of "x.x.x" format
+ * to 4-byte unsigned integer and vice versa.
+ * Most significant byte in integer is always 0.
  */
-uint32_t StringVersionToInt(const std::string& strVersion);
+class VersionInfo{
+    uint32_t nVersion;
+public:
+    VersionInfo(uint32_t version);
+    VersionInfo(const std::string& version);
 
+    VersionInfo(const VersionInfo&) = default;
+    VersionInfo(VersionInfo&&) = default;
+    VersionInfo& operator=(const VersionInfo&) = default;
 
-/**
- * @brief Converts version as 4-byte unsigned integer to string
- * @param nVersion 4-byte unsigned integer, most significant byte is always 0
- * @return version string in "x.x.x" format (last 3 bytes as version parts)
- * Throws std::bad_cast if format doesn\t match.
- */
-std::string IntVersionToString(uint32_t nVersion);
-
-
-/**
- * @brief Copy of the IntVersionToString, that returns "Invalid version" string
- * instead of throwing std::bad_cast
- * @param nVersion 4-byte unsigned integer, most significant byte is always 0
- * @return version string in "x.x.x" format (last 3 bytes as version parts)
- * or "Invalid version" if can't cast the given value
- */
-std::string SafeIntVersionToString(uint32_t nVersion);
-
+    operator uint32_t() const;
+    operator std::string() const;
+};
 
 #endif // HILUX_UTIL_H
